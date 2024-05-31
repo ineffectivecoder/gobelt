298c9d3c7cc96f3b10e79b7daa0ee680b94a467d
ineffectivecoder <r2d2@sostup.id>
2024-05-31 06:39:05 -0700

    added flag functionality for parameters

--- cmd/gobelt/main.go
+++ cmd/gobelt/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gobelt"
 )
@@ -9,10 +10,18 @@ func main() {
 	var key []string
 	var e error
 	fmt.Print("[+] Welcome to gobelt, the Golang alternative to Seatbelt\n")
-	key, e = gobelt.RDPHostQuery()
-	if e != nil {
-		fmt.Printf("Received an error %s", e.Error())
-	} else {
-		fmt.Printf("[+] The value of the registry key is %s", key)
+	// fun with flags
+	RDPHostQueryFlag := flag.Bool("RDPHostQuery", false, "Queries registry for successful RDP sessions performed by Current User")
+
+	flag.Parse()
+
+	if *RDPHostQueryFlag {
+		key, e = gobelt.RDPHostQuery()
+		if e != nil {
+			fmt.Printf("Received an error %s", e.Error())
+		} else {
+			fmt.Printf("[+] The value of the registry key is %s", key)
+		}
 	}
+
 }
